fc5fdc85561c36b191874c82ab9e181f550ef0e3
Thomas Warford <thomasdwarford@hotmail.co.uk>
2025-09-24 17:23:33 +0200

    job post-processing function (ugly, needs to be in package for jf-remote)

--- src/atomate2/vasp/flows/slabpes.py
+++ src/atomate2/vasp/flows/slabpes.py
@@ -8,8 +8,16 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
-from jobflow import Maker, Flow
+from jobflow import Maker, Flow, job
 from pymatgen.io.vasp.sets import SlabPESStaticSet
+import numpy as np
+
+from pathlib import Path
+from atomate2.vasp.powerups import update_user_incar_settings
+from atomate2.utils.path import strip_hostname
+from pymatgen.io.vasp import Outcar, Vasprun, Chgcar, Locpot
+from monty.os.path import zpath
+from ase.io import read, write
 
 from atomate2.vasp.jobs.matpes import MatPesGGAStaticMaker, MatPesMetaGGAStaticMaker
 from atomate2.common.jobs.utils import remove_workflow_files
@@ -20,6 +28,84 @@ if TYPE_CHECKING:
     from pathlib import Path
     from pymatgen.core import Structure
     from collections.abc import Sequence
+
+@job
+def post_process_slabpes(workdir_names, output_dir, uuids=None):
+    dataset_dir = Path(output_dir)
+    for i, workdir in enumerate(workdir_names):
+        workdir = Path(strip_hostname(workdir))
+        vasprun = Vasprun(zpath(workdir/'vasprun.xml'), parse_potcar_file=False)
+        if not vasprun.converged_electronic:
+            continue
+        outcar = Outcar(zpath(workdir/'OUTCAR'))
+        outcar.read_vacuum_potentials() # put vacuum_potential_upper, vacuum_potential_lower in outcar.data, if found
+        
+        id = vasprun.incar['SYSTEM']
+        xc_functional = 'R2SCAN' if 'METAGGA' in vasprun.incar else 'PBE'
+        has_dipole_correction = vasprun.incar.get('LDIPOL', False)
+
+        # get energy, stress, forces using ASE
+        atoms = read(zpath(workdir/'vasprun.xml'), -1)
+        atoms.info['ref_energy'] = atoms.get_total_energy()
+        atoms.info['ref_stress'] = atoms.get_stress(voigt=True)
+        atoms.arrays['ref_forces'] = atoms.get_forces()
+        atoms.calc = None
+
+
+        dipole = vasprun.ionic_steps[-1].get('dipole', None)
+
+        # Determine if custodian applied any corrections (i.e., job was restarted)
+        try:
+            entries = loadfn(zpath(workdir / "custodian.json"))
+            corrections = [c for e in entries for c in e.get("corrections", [])]
+            restart_count = len(corrections)
+        except Exception:
+            restart_count = -1
+
+        num_scf = len(vasprun.ionic_steps[-1]['electronic_steps'])
+
+        vacuum_potential_upper = outcar.data.get("vacuum_potential_upper", None)
+        vacuum_potential_lower = outcar.data.get("vacuum_potential_lower", None)
+        try: drift = np.array(outcar.drift[-1])
+        except: drift = None
+
+        efermi = vasprun.efermi
+        try: efermi_pmg = vasprun.calculate_efermi()
+        except: efermi_pmg = None
+        efield = vasprun.incar.get('EFIELD', None)
+
+        if dipole: atoms.info['dipole'] = dipole
+  
+        atoms.info['restart_count'] = restart_count
+        atoms.info['num_scf'] = num_scf
+        atoms.info['efermi'] = efermi
+        if efermi_pmg: atoms.info['efermi_pmg'] = efermi_pmg
+        if efield is not None: atoms.info['efield'] = efield
+        if vacuum_potential_upper: atoms.info['vacuum_potential_upper'] = vacuum_potential_upper
+        if vacuum_potential_lower: atoms.info['vacuum_potential_lower'] = vacuum_potential_lower
+        if drift: atoms.info['drift'] = drift
+        atoms.info['final_electronic_step'] = vasprun.ionic_steps[-1]['electronic_steps'][-1]
+        
+        # for tracking purposes
+        atoms.info['vasp_dir_name'] = str(workdir)
+        if uuids:
+            atoms.info['uuid'] = uuids[i]
+
+        # save
+        functional_dipole_label = xc_functional
+        if has_dipole_correction: functional_dipole_label += "_dipole"
+        out_dir = (dataset_dir / functional_dipole_label / id); out_dir.mkdir(parents=True, exist_ok=True)
+        write(out_dir / "labels.xyz.gz", atoms, append=True)
+
+        # chgcar stuff
+        c_stem = 'charge'
+        if efield is not None: c_stem += f'_e{efield}'
+        Chgcar.from_file(zpath(workdir/'CHGCAR')).to_cube(out_dir / f'{c_stem}.cube.gz')
+
+        # locpot stuff
+        l_stem = 'locpot'
+        if efield is not None: l_stem += f'_e{efield}'
+        Locpot.from_file(zpath(workdir/'LOCPOT')).to_cube(out_dir / f'{l_stem}.cube.gz')
 
 @dataclass
 class SlabPesStaticFlowMaker(Maker):
